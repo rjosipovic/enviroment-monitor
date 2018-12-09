ef8cf64ee9a6d145d49c0fb8f80e3af85bfa8f95
Roman Josipović <rjosipovic@gmail.com>
2018-12-09 18:02:18 +0100

    Refactoring Configuration service

---
+++ src/home/enviroment/config/Prop.java
@@ -0,0 +1,47 @@
+package home.enviroment.config;
+
+public enum Prop {
+	
+	ENVIROMENT_SERVICE_CONFIG_FILE("enviroment-service-config-file", "/etc/enviromental-monitor/enviroment-monitor.conf"),
+	ENVIROMENT_SERVICE_LOG_PROPS_FILE("enviroment-service-log-props-file", "/etc/enviroment-monitor/logging.properties"),
+	ENVIROMENT_SERVICE_APPLICATION_NAME("enviroment-service-application-name", "EnviromentMonitor"),
+	ENVIROMENT_SERVICE_APPLICATION_STATUS_TIMEOUT("enviroment-service-application-status-timeout", "5"),
+	
+	SENSE_MESUREMENT_SCRIPT_PATH("sense_mesurement_script_path", "/home/pi/sense-hat/log-enviroment.py"),
+	SENSE_MESUREMENT_TEMP_PATTERN("sense_mesurement_temp_pattern", "^(temp_calibrated:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$"),
+	SENSE_MESUREMENT_PRESSURE_PATTERN("sense_mesurement_pressure_pattern", "^(pressure:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$"),
+	SENSE_MESUREMENT_HUMIDITY_PATTERN("sense_mesurement_humidity_pattern", "^(humidity:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$"),
+	SENSE_MESUREMENT_PERSIST_FOLDER("sense_mesurement_persist_folder", "/tmp/"),
+	SENSE_MESUREMENT_MIN_FILE_RECORDS("sense_mesurement_min_file_records", "1000"),
+	
+	MONITOR_SERVER_IP("monitor-server-ip", "localhost"),
+	MONITOR_SERVER_PORT("monitor-server-port", "1234"),
+	MONITOR_MAX_WORKERS("monitor-max-workers", "10"),
+	MONITOR_SERVER_SOCKET_TO("monitor-server-socket-timeout", "2000"),
+	MONITOR_STATUS_MSG("monitor-status-msg", "EnviromentMonitor is RUNNING"),
+	
+	REMOTE_TRANSFER_HOST("remote-transfer-host", "localhost"),
+	REMOTE_TRANSFER_USER("remote-transfer-user", "roman"),
+	REMOTE_TRANSFER_PASS("remote-transfer-pass", "roman"),
+	REMOTE_TRANSFER_PORT("remote-transfer-port", "22"),
+	REMOTE_TRANSFER_CONNECT_TO("remote-transfer-connect-to", "5000"),
+	REMOTE_TRANSFER_FOLDER("remote-transfer-folder", "/tmp/"),
+	
+	JMX_SERVICE_URL("jmx-serice-url", "service:jmx:rmi:///jndi/rmi://:1099/jmxrmi");
+	
+	private String name;
+	private String defaultvalue;
+	
+	private Prop(String name, String defaultValue) {
+		this.name = name;
+		this.defaultvalue = defaultValue;
+	}
+	
+	public String getDefaultvalue() {
+		return defaultvalue;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
--- src/home/enviroment/services/ConfigurationService.java
+++ src/home/enviroment/services/ConfigurationService.java
@@ -1,24 +1,31 @@
 package home.enviroment.services;
 
+import home.enviroment.config.Prop;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.ConfigurationBuilderEvent;
+import org.apache.commons.configuration2.builder.ReloadingFileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.event.Event;
+import org.apache.commons.configuration2.event.EventListener;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.configuration2.reloading.PeriodicReloadingTrigger;
 
 import com.google.common.util.concurrent.AbstractIdleService;
 
-public class ConfigurationService extends AbstractIdleService {
+public class ConfigurationService extends AbstractIdleService implements EventListener<Event> {
 	
 	private static final Logger LOG = Logger.getLogger(ConfigurationService.class.getName());
 	
-	//SenseJob
-	private static final String SENSE_HAT_SCRIPT_PATH = "/home/pi/sense-hat/log-enviroment.py";
-	private static final String TEMP_PATTERN = "^(temp_calibrated:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$";
-	private static final String HUMIDITY_PATTERN = "^(humidity:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$";
-	private static final String PRESSURE_PATTERN = "^(pressure:)([-+]?([0-9]*\\.[0-9]+|[0-9]+))$";
-	private static final String SENSE_PERSIST_FOLDER = "/tmp/";
-	private static final int MAX_PERSISTED_ELEMENTS = 12;
-	//SenseJob
-	
 	private static ConfigurationService instance = null;
-	
+
 	public static synchronized ConfigurationService getInstance() {
 		if(instance == null) {
 			instance = new ConfigurationService();
@@ -26,45 +33,63 @@ public class ConfigurationService extends AbstractIdleService {
 		return instance;
 	}
 	
+	private ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration> builder;
+	private PeriodicReloadingTrigger trigger;
+	private Configuration config;
+	private String configFile;
+	
 	public ConfigurationService() {
+		configFile = System.getProperty("application.configuration");
 	}
 
 	@Override
 	protected void startUp() throws Exception {
-		LOG.info("About to start ...");
+		LOG.info(String.format("About to start ConfigurationService with file: %s", configFile));
+		ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+		Parameters params = new Parameters();
+		builder = new ReloadingFileBasedConfigurationBuilder<PropertiesConfiguration>(PropertiesConfiguration.class);
+		builder.configure(params.properties()
+				.setFileName(configFile));
+		builder.setAutoSave(Boolean.TRUE);
+		builder.addEventListener(ConfigurationBuilderEvent.RESET, this);
+		trigger = new PeriodicReloadingTrigger(builder.getReloadingController(), null, 1, TimeUnit.SECONDS, exec);
+		trigger.start();
+		config = builder.getConfiguration();
+		LOG.info(String.format("%s started", ConfigurationService.class.getSimpleName()));
 	}
 	
-	public void reloadConfiguration() {
-		LOG.info("Reloading configuration");
-		//TODO
+	public String getProperty(Prop name) {
+		LOG.fine(String.format("About to get property: {%s}", name.name()));
+		String propValue = config.getString(name.getName(), name.getDefaultvalue());
+		LOG.fine(String.format("Property: {%s} has value: {%s}", name.name(), propValue));
+		return propValue;
 	}
 	
-	public int getMaxPersistedElements() {
-		return MAX_PERSISTED_ELEMENTS;
+	public synchronized void setProperty(Prop name, String value) throws ConfigurationException {
+		LOG.fine(String.format("About to set property: {%s} with value: {%s}", name.name(), value));
+		config.setProperty(name.getName(), value);
 	}
 	
-	public String getSensePersistFolder() {
-		return SENSE_PERSIST_FOLDER;
+	private void shutdownTrigger() {
+		if(trigger != null && trigger.isRunning()) {
+			LOG.info("About to shutdown configuration reloading trigger");
+			trigger.shutdown();
+		}
 	}
 	
-	public String getSenseHatScriptPath() {
-		return SENSE_HAT_SCRIPT_PATH;
-	}
-	
-	public String getTempPattern() {
-		return TEMP_PATTERN;
-	}
-	
-	public String getHumidityPattern() {
-		return HUMIDITY_PATTERN;
-	}
-	
-	public String getPressurePattern() {
-		return PRESSURE_PATTERN;
+	@Override
+	protected void shutDown() throws Exception {
+		LOG.info("About to stop ConfigurationService");
+		shutdownTrigger();
 	}
 
 	@Override
-	protected void shutDown() throws Exception {
-		LOG.info("About to stop ...");
+	public void onEvent(Event event) {
+		LOG.info("Configuration reset triggered");
+		try {
+			config = builder.getConfiguration();		
+		} catch(ConfigurationException e) {
+			LOG.log(Level.SEVERE, "Unable to reload configuration", e);
+		}
 	}
 }
